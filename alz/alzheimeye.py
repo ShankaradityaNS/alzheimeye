a008ce68b1ef6c7515095cbb459f4f0111128f6e
7kami7 <65914913+7kami7@users.noreply.github.com>
2020-11-07 23:01:44 +0530

    Update alzheimeye.py

--- alz/alzheimeye.py
+++ alz/alzheimeye.py
@@ -1,48 +1,50 @@
-
-
-
-
-
 import cv2
 
-thres = 0.5
 
-cap = cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
-
-
-
-
-
-classNames=[]
-classFile='C:\\Users\\Renga Bashyam\\Desktop\\alz\\coco.txt'
-with open(classFile,'rt') as f:
-    calssNames=f.read().rstrip('\n').split('\n')
-configPath = 'C:\\Users\\Renga Bashyam\\Desktop\\alz\\yolo3.cfg'
-weightsPath = 'C:\\Users\\Renga Bashyam\\Desktop\\alz\\yolov3.weights'
-
-net = cv2.dnn_DetectionModel(weightsPath,configPath)
-net.setInputSize(320,320)
-net.setInputScale(1.0/127.5)
-net.setInputMean(127.5,127.5,127.5)
-net.setInputSwapRB(True)
-
-while True:
-    success,img=cap.read()
-    classIds , confs,bbox = net.detect(img,confThreshold=thres)
-    print(classIds,bbox)
+def main():
     
-    if len(classIds) != 0:
+
+    thres = 0.5
     
-        for classId,confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-            cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-            cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
-            cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+    cap = cv2.VideoCapture(0)
+    cap.set(3,640)
+    cap.set(4,480)
+    
+    
+    
+    
+    
+    classNames=[]
+    classFile='C:\\Users\\Renga Bashyam\\Desktop\\alz\\coco.names'
+    with open(classFile,'rt') as f:
+        classNames=f.read().rstrip('\n').split('\n')
+    configPath = 'C:\\Users\\Renga Bashyam\\Desktop\\alz\\yolo3.cfg'
+    weightsPath = 'C:\\Users\\Renga Bashyam\\Desktop\\alz\\yolov3.weights'
+    
+    net = cv2.dnn_DetectionModel(weightsPath,configPath)
+    net.setInputSize(320,320)
+    net.setInputScale(1.0/127.5)
+    net.setInputMean(127.5)
+    net.setInputSwapRB(True)
+    
+    while True:
+        success,img=cap.read()
+        classIds , confs,bbox = net.detect(img,confThreshold=thres)
+        print(classIds,bbox)
+        
+        if len(classIds) != 0:
+        
+            for classId,confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+                cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                cv2.putText(img,classNames[classId].upper(),(box[0]+10,box[1]+30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+                cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+            
+            
         
         
-    
-    
-    
-    cv2.imshow('Output',img)
-    cv2.waitkey(1)
+        
+        cv2.imshow('Output',img)
+        cv2.waitKey(1)
+        
+if __name__=='__main__':
+    main()
